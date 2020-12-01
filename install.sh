42f8da621e09a10019f78b8935aab15906b7447a
Vishal Gauba <vishal.gauba@gmail.com>
2020-12-01 22:56:19 +0530

    feat(bash/git): add git contribs completion and prompt

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ bash $HOME/dotfiles/vim/install
 bash $HOME/dotfiles/tmux/install
 bash $HOME/dotfiles/git/install
 bash $HOME/dotfiles/nodejs/install
+bash $HOME/dotfiles/docker/install
+# bash $HOME/dotfiles/zsh/install

6ce25aedba9b366b890f58967408e752b3af3f43
Vishal Gauba <vishal.gauba@gmail.com>
2020-12-01 19:56:27 +0530

    feat: add ssh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+bash $HOME/dotfiles/ssh/install
+bash $HOME/dotfiles/bash/install
 bash $HOME/dotfiles/brew/install
 bash $HOME/dotfiles/vim/install
-bash $HOME/dotfiles/bash/install
 bash $HOME/dotfiles/tmux/install
